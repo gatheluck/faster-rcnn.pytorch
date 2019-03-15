3d9e80a49f4121e099e150e01cd6b2cb815a518f
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-15 15:11:09 +0900

    update generator

--- abci/generator_main.sh
+++ abci/generator_main.sh
@@ -1,15 +1,17 @@
 models=(resnet50 resnet101)
 train_types=(IN IN-FGSM IN-HALF IN-IN FGSM FGSM-IN FGSM-HALF FGSM-FGSM)
-where=aist  #(aist mlab)
+where=mlab  #(aist mlab)
 
 if [ ${where} = aist ]; then
+	venv=anaconda3
 	logdir=~/user/waseda/abci_log
 	projectdir=/home/aaa10329ah/user/waseda/faster-rcnn
 	gpu_ids=0,1,2,3
 elif [ ${where} = mlab ]; then
-	logdir=~
-	projectdir=~
-	gpu_ids=0
+	venv=miniconda3
+	logdir=~/iccv2019/abci_log
+	projectdir=~/iccv2019/faster-rcnn
+	gpu_ids=0,1,2,3
 else
 	echo 'Invalid' 1>&2
   exit 1
@@ -36,7 +38,7 @@ for model in ${models[@]}; do
 		fi
 
 		echo -e "#!/bin/bash\n\n#$ -l rt_F=1\n#$ -l h_rt=24:00:00\n#$ -j y\n#$ -N ${name}\n#$ -o ${logpath}\n\n" > ${filename}
-		echo -e "source /etc/profile.d/modules.sh\nmodule load cuda/9.0/9.0.176.4\nexport PATH=\"~/anaconda3/bin:\${PATH}\"\nsource activate ${env_name}\n" >> ${filename}
+		echo -e "source /etc/profile.d/modules.sh\nmodule load cuda/9.0/9.0.176.4\nexport PATH=\"~/miniconda3/bin:\${PATH}\"\nsource activate ${env_name}\n" >> ${filename}
 
 		echo -e "cd ${projectdir}" >>  ${filename}
 		echo -e "python run_all.py \
